048eb978d883ea8b339888b11e53a737e0f69ebb
KobinaOtc <kobinaotchereadjoku@gmail.com>
2026-03-31 19:59:37 +0000

    update

--- swagger.js
+++ swagger.js
@@ -7,6 +7,19 @@ const doc = {
     },
     host: 'localhost:8080',
     schemes: ['http', 'https'],
+
+    // Add security definistions below
+    securityDefinitions: {
+        oAuthSamaple: {
+            type: 'oauth2',
+            authorizationUrl: 'https://github.com/login/oauth/authorize',
+            flow: 'implicit',
+            scopes: {
+                'read_pets': 'read your pets',
+                'write_pets': 'modify pets in your account'
+            }
+        }
+    }
 };
 
 const outputFile = './swagger.json';
